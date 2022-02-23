86933720ff0ceb8575ed639f89e54db933b48c30
Gregory Wainer <greg.wainer@gmail.com>
2022-02-23 17:45:15 -0500

    removed unused files

--- mac/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-open "http://127.0.0.1:3000"
-python3 ./rt_data_server/websocket_server.py & python3 -m http.server ./rt_data_server/ && fg
--- windows/start.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-start "" localhost:8000
-python3 -m http.server
