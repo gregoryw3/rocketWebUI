28636bb1cfa5282da5b94a80ab70b9cf49350042
Gregory Wainer <greg.wainer@gmail.com>
2022-04-06 11:27:55 -0400

    almost final

--- clientRocket/websocket_server.py
+++ clientRocket/websocket_server.py
@@ -4,26 +4,20 @@ import os
 import random
 import socket
 import time
-from abc import ABC
-from glob import glob
-from multiprocessing import Pipe, Process, Queue
 
 from tornado import ioloop, web, websocket
 
 host = "127.0.0.1"
 port = 55021
 
-
-def open_socket():
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    except socket.error as err:
-        print("socket creation failed, for why?")
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error as err:
+    print("socket creation failed, for why?")
     s.bind(host, port)
-    return s
 
 
-class WebSocketHandler(websocket.WebSocketHandler, ABC):
+class WebSocketHandler(websocket.WebSocketHandler):
     # Addition for Tornado as of 2017, need the following method
     # per: http://stackoverflow.com/questions/24851207/tornado-403-get-warning-when-opening-websocket/25071488#25071488
     def check_origin(self, origin):
@@ -54,34 +48,54 @@ class WebSocketHandler(websocket.WebSocketHandler, ABC):
         return True
 
     # Our function to send new (random) data for charts
-    def send_data(self, start_time, past_height, web_message):
+    def send_data(self, start_time, web_message):
         print("Sending Data")
 
         # create a bunch of random data for various dimensions we want
-        currentHeight = random.randrange(1, 300)
+        current_height = random.randrange(1, 300)
 
-        # create a new data point
-        point_data = {
-            'Time': (round((time.time() - start_time), 4)),
-            'Height': currentHeight + past_height,
-            'AirPressure': random.randrange(1, 100),
-            'Humidity': random.randrange(1, 100),
-            'Temperature': random.randrange(1, 100),
-        }
+        radio_message = s.recv()
+        radio_message = parse_radio(radio_message)
 
-        past_height = currentHeight + past_height
-
-        print(point_data)
+        print(radio_message)
 
         # write the json object to the socket
         if web_message:
-            self.write_message(json.dumps(point_data))
+            self.write_message(json.dumps(radio_message))
 
         time.sleep(1)
 
         # create new ioloop instance to intermittently publish data
         ioloop.IOLoop.instance().add_timeout(datetime.timedelta(seconds=1),
-                                             self.send_data(start_time, past_height, web_message))
+                                             self.send_data(start_time, web_message))
+
+
+def parse_radio(radio_message):
+    data = radio_message.decode('utf-8')
+    data = data.split(';')
+    pr_alt_data = data[0].split(',')
+    t_h_data = data[1].split(',')
+    lat_long__time_data = data[2].split(',')
+
+    pressure = int(pr_alt_data[0])
+    altitude = int(pr_alt_data[1])
+    humidity = int(t_h_data[1])
+    temperature = int(t_h_data[0])
+    latitude = int(lat_long__time_data[0])
+    longitude = int(lat_long__time_data[1])
+    gps_time = float(lat_long__time_data[2])
+
+    point_data = {
+        'Time': gps_time,
+        'Height': altitude,
+        'AirPressure': pressure,
+        'Humidity': humidity,
+        'Temperature': temperature,
+        'latitude': latitude,
+        'longitude': longitude
+    }
+
+    return point_data
 
 
 def main():
@@ -90,7 +104,6 @@ def main():
                                    (r'/websocket', WebSocketHandler)])
     application.listen(8001)
     ioloop.IOLoop.instance().start()
-    open_socket()
 
 
 if __name__ == "__main__":
