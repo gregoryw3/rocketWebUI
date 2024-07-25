14bcf2d3d4e695d21036c88a7651dfc2f4f01792
Gregory Wainer <greg.wainer@gmail.com>
2024-07-25 06:50:13 -0400

    typos

--- clientRocket/client.py
+++ clientRocket/client.py
@@ -6,19 +6,20 @@ from digi.xbee.devices import XBeeDevice
 # In UNIX-based systems, the port usually starts with /dev/tty.
 #
 # Instantiate a local XBee object.
-# Port and Baude Rate
+# Port and Baud Rate
 xbee = XBeeDevice("/dev/tty.usbserial-DA01HY9O", 57600)
 
-
-#The default timeout to wait for the send status is two seconds. 
-#However, you can configure the timeout using the get_sync_ops_timeout() and set_sync_ops_timeout() methods of an XBee class.
+# The default timeout to wait for the send status is two seconds. However, you can configure the timeout using the
+# get_sync_ops_timeout() and set_sync_ops_timeout() methods of an XBee class.
 xbee.open()
+
 
 # Define callback.
 def my_data_received_callback(xbee_message):
     address = xbee_message.remote_device.get_64bit_addr()
     data = xbee_message.data.decode("utf8")
     send_to_web("Received data from %s: %s" % (address, data))
+
 
 def send_to_web(child_conn, message):
     child_conn.send(message)
@@ -27,9 +28,9 @@ def send_to_web(child_conn, message):
 
 # Add the callback.
 xbee.add_data_received_callback(my_data_received_callback)
-# This mechanism for reading data does not block your application. 
-# Instead, you can be notified when new data has been received if you are subscribed or registered 
-# to the data reception service using the add_data_received_callback() method with a data reception callback as parameter.
+# This mechanism for reading data does not block your application. Instead, you can be notified when new data has
+# been received if you are subscribed or registered to the data reception service using the
+# add_data_received_callback() method with a data reception callback as parameter.
 #
 # To stop listening to new received data, use the del_data_received_callback() method 
 # to unsubscribe the already-registered callback.
